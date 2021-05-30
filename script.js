77258642756d1f731013868af3872707cb639a63
Jan Joseph <jan00joseph@gmail.com>
2021-05-30 18:49:15 +0530

    addDot fn changed to fix Dot operation addition bug

--- script.js
+++ script.js
@@ -8,24 +8,28 @@ const screen = document.querySelector(".screen");
 
 let currentOperation = '';
 let previousOperand = 0, currentOperand;
-let screenRetain = true;
+let screenRetain = false;
 
 window.addEventListener('keydown', findKey);
 btnNumbers.forEach(btn => btn.addEventListener('click',() => addNumber(btn.textContent)));
 btnOperators.forEach(btn => btn.addEventListener('click', () => setOperator (btn.textContent)))
 btnEqual.addEventListener('click',() => evaluate())
 btnClear.addEventListener('click', () => clear());
+btnDelete.addEventListener('click',() => deleteNumber());
+btnDot.addEventListener('click', () => addDot());
+
 function findKey(e) {
     if (e.key >= 0 && e.key <= 9) addNumber(e.key);
     if (e.key === ".") addDot();
     if (e.key === "=" || e.key === "Enter") evaluate();
-    if (e.key === "Backspace") deleteNumber(e.key);
+    if (e.key === "Backspace") deleteNumber();
     if (e.key === "Escape") clear();   
     if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/") setOperator(e.key);
 }
 
 function addNumber(n) {
-    if(screen.textContent == '0' || !screenRetain) {
+    if(screen.textContent === '0' || !screenRetain) {
+        console.log("Some number");
         setScreen(n);
         screenRetain = true;
     } else {
@@ -65,7 +69,24 @@ function evaluate() {
 function setScreen(n) {
     screen.textContent = n;
 }
+
+function deleteNumber() {
+    let result = screen.textContent.slice(0, -1);
+    setScreen(result);
+    currentOperand = result;
+}
+function addDot() {
+    if (screen.textContent.includes(".") && screenRetain) return;
+    if (!screenRetain) {
+        console.log("decimal");
+        screenRetain = true;
+        setScreen("0.")
+        console.log(screen.textContent);
+        return;
+    }
+    setScreen(screen.textContent + ".");
+}
 const add = (a,b) => a+b;
 const subtract = (a,b) => a-b;
 const multiply = (a,b) => a*b;
-const divide = (a,b) => (b === 0)?alert("Divide by 0 is not possible"): a/b;
+const divide = (a,b) => (b === "0") ? alert("Divide by 0 is not possible"): a/b;
